9345a45bd6a6d98ff5eaec3b412eb65a1abd29c1
Yisong Zou <yz558@vcm-12330.vm.duke.edu>
2020-01-21 20:27:11 -0500

    finish

---
+++ my_malloc/my_malloc.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+
+//First Fit malloc/free
+void *ff_malloc(size_t size);
+void ff_free(void *ptr);
+
+//Best Fit malloc/free
+void *bf_malloc(size_t size);
+void bf_free(void *ptr);
+
+unsigned long get_data_segment_size(); //in bytes
+unsigned long get_data_segment_free_space_size(); //in bytes
